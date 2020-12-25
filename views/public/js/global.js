423a85215e2146df4cc0c9d86d93e5b47e32a12f
x14102004x <marcosproenca144@gmail.com>
2020-12-25 19:15:52 -0300

    Altered Users ON-OFF

--- views/public/js/global.js
+++ views/public/js/global.js
@@ -1,18 +1,32 @@
 "use strict";
-var $ = function (elm) {
+var _ = function (elm) {
     var e = document.querySelector(elm);
+    var es = document.querySelectorAll(elm);
     var querys = function (elm, index) {
         return document.querySelectorAll(elm)[index];
+    };
+    var html = function (elmHtml) {
+        es.forEach(function (elm) {
+            elm.innerHTML = elmHtml;
+        });
+    };
+    var text = function (txt, i) {
+        e.innerText = txt;
+        // es.forEach((elm) => {
+        //   (<HTMLElement>elm).innerText = txt;
+        // });
     };
     var child = function (configElement) {
         var children = document.createElement(configElement.Element);
         children.className = configElement.Class;
-        children.textContent = configElement.Content ? configElement.Content : "";
+        children.textContent = configElement.Content;
         if (!configElement.Parent)
             e.appendChild(children);
         else
-            querys(configElement.Parent, configElement.Index).appendChild(children);
+            configElement.Index != undefined
+                ? querys(configElement.Parent, configElement.Index).appendChild(children)
+                : false;
         return { child: child };
     };
-    return { child: child };
+    return { child: child, html: html, text: text };
 };
