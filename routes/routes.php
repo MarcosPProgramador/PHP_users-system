879e470c45360a65e22690b86a66e9addf1da0b6
x14102004x <marcosproenca144@gmail.com>
2021-03-20 16:41:18 -0300

    Updated Routes

--- routes/routes.php
+++ routes/routes.php
@@ -1,13 +1,25 @@
 <?php
 class routes {
     public function __construct() {
-
         $api = simpleTasks::Class('api');
 
         $this->authController($api);
+        $this->userController($api);
+        $this->commentController($api);
+    }
+
+    private function commentController(object $api) {
+        $api::get('/api/comments', "commentController@index");
+
+    }
+
+    private function userController(object $api) {
+        $api::get('/api/users/online', 'onUserController@index');
+        $api::get('/api/users/offline', 'offUserController@index');
+
     }
 
     private function authController(object $api) {
-        $api::post('/auth', 'authController@authenticate');;
+        $api::get('/auth', 'authController@authenticate');
     }
 }
