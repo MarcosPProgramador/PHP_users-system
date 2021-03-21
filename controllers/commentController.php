16e773efea7151c79d38c0e4c8b9862a68b5d4f5
x14102004x <marcosproenca144@gmail.com>
2021-03-21 18:10:30 -0300

    Added Controller: Comment

--- controllers/commentController.php
+++ controllers/commentController.php
@@ -1,30 +1,10 @@
 <?php
 class commentController {
     public function index() {
+        $commentRepository = tasks::Class('commentRepository');
         return [
             'code'  => 200,
-            'datas' => [
-                [
-                    'name'       => 'marcos',
-                    'comment'    => 'lorem ipsum in dolor aliquid',
-                    'lastaction' => '2021-01-05 15:11:03',
-                ],
-                [
-                    'name'       => 'pedro',
-                    'comment'    => 'lorem ipsum in dolor aliquid',
-                    'lastaction' => '2021-01-05 15:11:03',
-                ],
-                [
-                    'name'       => 'josé',
-                    'comment'    => 'lorem ipsum in dolor aliquid',
-                    'lastaction' => '2021-01-05 15:11:03',
-                ],
-                [
-                    'name'       => 'joão',
-                    'comment'    => 'lorem ipsum in dolor aliquid',
-                    'lastaction' => '2021-01-05 15:11:03',
-                ],
-            ],
+            'datas' => $commentRepository->findComments(),
         ];
     }
 }
