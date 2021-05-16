ca5582c76295fca77f9c2577b4b35f5f8e4863e7
x14102004x <marcosproenca144@gmail.com>
2021-05-16 19:59:16 -0300

    refactor: if not loaded, show loading component

--- mobile/src/App.tsx
+++ mobile/src/App.tsx
@@ -2,12 +2,17 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { View } from 'react-native';
 import 'react-native-gesture-handler';
+import useFonts from './hooks/useFonts';
 import TabsRoutes from './routes/tabs.routes';
 
 /* eslint-disable-next-line prettier/prettier */
-/* eslint-disable-line */ console.log('test...');
+/* eslint-disable-line */
 
 const App: React.FC = () => {
+  const { loaded, loading: Loading } = useFonts();
+
+  if (!loaded) return <Loading />;
+
   return (
     <>
       <View style={{ flex: 1, backgroundColor: '#312e38' }}>
