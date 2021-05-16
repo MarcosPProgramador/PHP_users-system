0c36eb8d2f032b1a7e3ed94c6fe00e0b3e05b781
x14102004x <marcosproenca144@gmail.com>
2021-05-16 20:00:37 -0300

    feat: the loading component was created with your style

---
+++ mobile/src/components/Loading/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text } from 'react-native';
+import { Container } from './styles';
+
+const Loading: React.FC = () => {
+  return (
+    <Container>
+      <Text>Loading...</Text>
+    </Container>
+  );
+};
+export default Loading;
